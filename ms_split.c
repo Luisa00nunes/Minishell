733146b7d5f6ca93d7806e3faa31397898215723
Luisa00nunes <luisaearthur@gmail.com>
2025-09-23 01:15:35 -0300

    lexer encaminhado

--- ms_split.c
+++ ms_split.c
@@ -6,9 +6,72 @@
 /*   By: ldos_sa2 <ldos-sa2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 14:36:27 by ldos_sa2          #+#    #+#             */
-/*   Updated: 2025/09/18 09:23:47 by ldos_sa2         ###   ########.fr       */
+/*   Updated: 2025/09/23 00:20:31 by ldos_sa2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
+// primeiro separar em pipe
+// com aspas
+// red in red out append e heredoc
+static int	count_process(char const *s)
+{
+	int	i;
+	int	process;
+	int pipes;
+
+	i = 0;
+	process = 0;
+	pipes = 0;
+	while (s[i])
+	{
+		if (s[i] != '|')
+		{
+			process++;
+			while (s[i] != '|' && s[i])
+				i++;
+		}
+		else
+			pipes++;
+			i++;
+	}
+	return (process + pipes);
+}
+
+void	split_process(char *prompt)
+{
+	int i = 0;
+	char	**process_list;
+	char	**process;
+	t_node	*ls = NULL;
+	t_node	*node;
+
+	process_list = ft_split(prompt, '|');
+	while(i < count_process(prompt))
+	{
+		process = ft_split(process_list[i], ' ');
+		//crio os tokens
+		//limpo a lista process
+		i++;
+	}
+	/*i = 0;
+	while(ls)
+	{
+		printf("Type: %i   Value: %s\n", ls->type, ls->value);
+		ls = ls ->next;
+	}*/
+}
+int	main()
+{
+	char *prompt;
+
+	while(1)
+	{
+		prompt = readline("minishell > ");
+		split_process(prompt);
+		printf("%s\n", prompt);
+	}
+	return (0);
+}
--- ms_test.c
+++ ms_test.c
@@ -6,7 +6,7 @@
 /*   By: ldos_sa2 <ldos-sa2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 08:18:07 by ldos_sa2          #+#    #+#             */
-/*   Updated: 2025/09/18 13:02:32 by ldos_sa2         ###   ########.fr       */
+/*   Updated: 2025/09/23 01:07:33 by ldos_sa2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ t_node	*node_last(t_node *node)
 	if(!node)
 		return (NULL);
 	temp = node;
-	while (temp->next != NULL) //enquanto o pont next n estiver apont. para o null
+	while (temp->next != NULL)
 		temp = temp->next; //o ponteiro temp vai "virar" o ponteiro next
 	return(temp);
 }
@@ -67,31 +67,21 @@ void	nodeadd_back(t_node **lst, t_node *new)
 	else //se head tiver apontando para o null
 		*lst = new;
 }
-void	split_tokens(char *prompt)
+void	split_tokens(char *process)
 {
-	int i = 0;
-	char	**list;
 	t_node	*ls = NULL;
 	t_node	*node;
 
-	list = ft_split(prompt, ' ');
-	while (i < count_words(prompt, ' '))
+	if(!ft_strncmp(process, "|", 1))
 	{
-		if(!ft_strncmp(list[i], "|", 2))
-		{
-			node = newnode(list[i], PIPE);
-			nodeadd_back(&ls,node);
-		}
-		else
-		{
-			node = newnode(list[i], WORD);
-			nodeadd_back(&ls, node);
-		}
-		free(list[i]);
-		i++;
+		node = newnode(process, PIPE);
+		nodeadd_back(&ls, node);
 	}
-	free(list);
-	i = 0;
+	else
+	{
+		node = newnode(process, WORD);
+		nodeadd_back(&ls, node);
+	}
 	while(ls)
 	{
 		printf("Type: %i   Value: %s\n", ls->type, ls->value);
@@ -100,6 +90,53 @@ void	split_tokens(char *prompt)
 
 }
 
+static int	count_process(char const *s)
+{
+	int	i;
+	int	process;
+	int pipes;
+
+	i = 0;
+	process = 0;
+	pipes = 0;
+	while (s[i])
+	{
+		if (s[i] != '|')
+		{
+			process++;
+			while (s[i] != '|' && s[i])
+				i++;
+		}
+		else
+			pipes++;
+			i++;
+	}
+	return (process + pipes);
+}
+
+void	split_process(char *prompt)
+{
+	int i = 0;
+	int j = 0;
+	char	**process_list;
+	char	**process;
+
+	process_list = ft_split(prompt, '|');
+	while(i < count_process(prompt))
+	{
+		process = ft_split(process_list[i], ' ');
+		while(process[j])
+		{
+			split_tokens(process[j]);
+			j++;
+		}
+		free(process);
+		split_tokens("|");
+		j = 0;
+		i++;
+	}
+	free(process_list);
+}
 int	main()
 {
 	char *prompt;
@@ -107,7 +144,7 @@ int	main()
 	while(1)
 	{
 		prompt = readline("minishell > ");
-		split_tokens(prompt);
+		split_process(prompt);
 		printf("%s\n", prompt);
 	}
 	return (0);
